97f89befd02fb97290315cd27ea8a070ab3b760c
Andrés Melendres <amelendresy@outlook.com>
2023-09-13 22:29:22 -0500

    Bind parameters

--- add.php
+++ add.php
@@ -1,15 +1,23 @@
 
 <?php
+  require ("database.php");
+  $error = null;
   if($_SERVER["REQUEST_METHOD"]=="POST"){
-    require ("database.php");
-    $name = $_POST["name"];
-    $phoneNumber = $_POST["phone_number"];
-    
-    $statement = $conn->prepare("insert into contacts(name, phone_number) values ('$name','$phoneNumber')");
-    $statement->execute();
-
+    if(empty($_POST["name"]) || empty($_POST["phone_number"])){
+      $error = "Los campos no deben estar vacíos";
+    }elseif(strlen($_POST["phone_number"]) < 9){
+      $error = "El número de telefono debe ser de 9 digitos";
+    }
+    else{
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
+      
+      $statement = $conn->prepare("insert into contacts(name, phone_number) values (:name,:phone_number)");
+      $statement->bindParam(":name",$_POST["name"]);
+      $statement->bindParam(":phone_number",$_POST["phone_number"]);
+      $statement->execute();
+    }
   }
-
 ?>
 
 <!DOCTYPE html>
@@ -73,12 +81,21 @@
           <div class="card">
             <div class="card-header">Add New Contact</div>
             <div class="card-body">
+              <?php
+              if($error != null){
+              ?>
+                <div class="text-danger">
+                <?= $error ?>
+                </div>
+              <?php
+                }
+              ?>
               <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
     
                   <div class="col-md-6">
-                    <input id="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="form-control" name="name"  autocomplete="name" autofocus>
                   </div>
                 </div>
     
@@ -86,7 +103,7 @@
                   <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
     
                   <div class="col-md-6">
-                    <input id="phone_number" type="tel" class="form-control" name="phone_number" required autocomplete="phone_number" autofocus>
+                    <input id="phone_number" type="tel" class="form-control" name="phone_number"  autocomplete="phone_number" autofocus>
                   </div>
                 </div>
     
