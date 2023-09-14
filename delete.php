5dab6b2a154b42c9fc9ec8698be7a3f5d25364ff
Andrés Melendres <amelendresy@outlook.com>
2023-09-14 00:07:58 -0500

    Delete corregido

--- delete.php
+++ delete.php
@@ -1,20 +1,20 @@
 <?php
 
-require ("database.php");
+require "database.php";
 
 $id = $_GET["id"];
-$statement = $conn -> prerare("select * from contacts where id = :id ");
+$statement = $conn->prepare("select * from contacts where id = :id ");
 $statement->execute([":id"=>$id]);
-if($statement->rowCount()==0){
+ if($statement->rowCount()==0){
   http_response_code(404);
   echo("HTTP 404 NOT FOUND");
   return;
-}
+} 
 
-$conn -> prerare("detele * from contacts where id = :id ")->execute([":id"=>$id]);
-// $statement = $conn -> prerare("detele * from contacts where id = :id ");
-/* $statement->bindParam(":id",$id);
-$statement->excecute(); */
+$conn -> prepare("delete from contacts where id = :id ")->execute([":id"=>$id]);
+/* $statement = $conn->prepare("delete from contacts where id = :id ");
+$statement->bindParam(":id",$id);
+$statement->execute();  */
 // $statement->execute([":id"=>$id]);
 
 
