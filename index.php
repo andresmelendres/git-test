89e88e7ce3840c356d4b8d27c87d6d9e30b0bfc5
Andrés Melendres <amelendresy@outlook.com>
2023-09-15 01:11:53 -0500

    Partials

--- index.php
+++ index.php
@@ -4,61 +4,10 @@
   $contacts = $conn->query("select * from contacts");
 ?>
 
+<?php 
+require "./partials/header.php";
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Boostrap -->
-  <link rel="stylesheet" 
-  href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.1/darkly/bootstrap.min.css" 
-  integrity="sha512-TW3EkvQFl2hehLxEL4TSEmP960SIWeTAdT6MWTdhbRdRyBRmQjEiWpoDW5VpgZQRy5RJE2qhugkeISF3BtDd1w==" 
-  crossorigin="anonymous" 
-  referrerpolicy="no-referrer" 
-  />
-  <script 
-  defer
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
-  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
-  crossorigin="anonymous">
-  </script>
-
-  <!-- Static Content -->
-  <link rel="stylesheet" href="static/css/index.css">
-  <title>Contacs App</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand font-weight-bold" href="#">
-        <img class="mr-2" src="./static/img/logo.png" />
-        ContactsApp
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="./index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./add.php">Add Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main>
     <div class="container pt-4 p-3">
 
       <div class="row">
@@ -82,7 +31,7 @@
               <div class="card-body">
                 <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
                 <p class="m-2"><?= $contact["phone_number"] ?></p>
-                <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
+                <a href="edit.php?id=<?=$contact["id"]?>" class="btn btn-secondary mb-2">Edit Contact</a>
                 <a href="delete.php?id=<?=$contact["id"]?>" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
@@ -93,8 +42,6 @@
       </div>
 
     </div>
-  </main>
-
-
-</body>
-</html>
+<?php
+require "./partials/footer.php";
+?>
